d4a74c9adc427f24ab9bb037b6f5e27451786ad5
manueleferreira <manueleferreira@gmail.com>
2016-06-25 12:38:53 -0300

    #1 refactor core function e build

--- bin/com/fnexercise/fnexercise.clj
+++ bin/com/fnexercise/fnexercise.clj
@@ -1,7 +1,8 @@
-(ns com.fnexercise.fnexercise)
-  (use 'clojure.java.io)
+(ns com.fnexercise.fnexercise
+  (:gen-class)
+  (:require [clojure.contrib.math :as math]
+            [clojure.java.io :as io] ))
   (use '[clojure.string :only (split)])
-  (use 'clojure.contrib.math)
 
 (definterface INode
   (getCar [])
@@ -9,7 +10,8 @@
   (setCar [x])
   (setCdr [x]))
 
-(deftype Node [^:volatile-mutable car ^:volatile-mutable cdr]
+(deftype Node [^:volatile-mutable car 
+               ^:volatile-mutable cdr]
   INode
   (getCar [this] car)
   (getCdr [this] cdr)
@@ -21,44 +23,37 @@
 
 (defn find-customer 
   "Find customer by number value."
-  {:added "1.0"
-   :static true}
-  [param] 
+  [number] 
   (loop [i 0]
     (when (< i (.length customers))
-      (let [node (nth customers i)]
+      (let [customer (nth customers i)]
         (cond
-          (= (.getCar node) param) node
+          (= (.getCar customer) number) customer
           :else (recur (inc i)))))))
 
-(defn add-customer-node 
+(defn add-new-customer 
   "Add new node to customer list."
-  {:added "1.0"
-   :static true}
-  [node]
+  [customer]
   (def customers 
-    (conj customers node)))
+    (conj customers customer)))
 
 
 (defn add-new-invite
   "Add new invite."
-  {:added "1.0"
-   :static true}
   [x y]
   (if (nil? (find-customer y))
-    (let [node-x (find-customer x) node-y (Node. y nil)] 
+    (let [node-x (find-customer x) 
+          node-y (Node. y nil)] 
       (do
         (if (nil? node-x) 
-          (add-customer-node (Node. x (list node-y)))
+          (add-new-customer (Node. x (list node-y)))
           (.setCdr node-x (conj (.getCdr node-x) node-y))) 
-        (add-customer-node node-y)))))
+        (add-new-customer node-y)))))
 
 (defn read-invite-file 
   "Read invites by customers from text file."
-  {:added "1.0"
-   :static true}
   [path]
-  (with-open [rdr (reader path)]
+  (with-open [rdr (io/reader path)]
     (doseq [line (line-seq rdr)]
       (let [list (split line #"\s")] 
         (do
@@ -67,29 +62,28 @@
 
 (defn list-ranking 
   "List Ranking values from customer map."
-  {:added "1.0"
-   :static true}
   []
   (do
     (def map-reward {})
     (loop [i 0]
       (when (< i (.length customers))
-        (let [customer (nth customers i) points (count-points-ranking customer)]
+        (let [customer (nth customers i) 
+              points (count-points-ranking customer)]
           (do
             (assoc map-reward customer points)
             (recur (inc i))))))))
 
 (defn count-points-ranking 
   "map customer and reward value."
-  {:added "1.0"
-   :static true}
+  {}
   [customer level]
   (let [invites (.getCdr customer)]
     (loop [i 0]
       (when (< i (.length invites))
-        (let [invite (nth invites i) chidren-invite (.getCdr invite)]
+        (let [invite (nth invites i) 
+              chidren-invite (.getCdr invite)]
           (if (no-nil? chidren-invite)
-            (+ (expt (/ 1 2) level) 
+            (+ (math/expt (/ 1 2) level) 
                (count-points-ranking invite (inc level)))))))))
 
 
--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.fnexercise/fnexercise "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Reward system"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
