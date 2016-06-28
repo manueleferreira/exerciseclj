d2fd7814484fcda2b77dfec9cb3ec7e3eabbf357
manueleferreira <manueleferreira@gmail.com>
2016-06-28 01:40:58 -0300

    #1 refactor loop functions

--- bin/com/fnexercise/core.clj
+++ bin/com/fnexercise/core.clj
@@ -31,65 +31,77 @@
 (defn find-customer 
   "Find customer by number value."
   [number] 
-  (loop [i 0]
-    (when (< i (.length customers))
-      (let [customer (nth customers i)]
-        (cond
-          (= (.getCar customer) number) customer
-          :else (recur (inc i)))))))
+  (first
+    (for [customer customers
+       :let [value (.getCar customer)]
+       :when (= value number)]
+    customer)))
+
+(defn has-already-invited
+  [y]
+  (for [customer customers
+        :let [customer-value (.getCar customer) 
+              children (.getCdr customer)]
+        :when (for [x children
+                    :let [value (.getCar x)]
+                    :when (or
+                            (= value y)
+                            (= customer-value y))]
+                           value)]
+    customer))
 
 (defn add-new-customer 
   "Add new node to customer list."
   [customer]
-  (nil? 
+  (nil?
     (def customers 
       (conj customers customer))) nil)
 
 (defn add-new-invite
   "Add new invite."
   [x y]
-    (let [node-x (find-customer x)
-          node-y (if (nil? (find-customer y))
-                   (Node. y false nil)
-                   nil)
-          list-y (if (not (nil? node-y))
-                   (list node-y))]
-      (do
-        (if (nil? node-x)
-          (add-new-customer (Node. x true list-y))
-          (do
-            (if (not (nil? node-y))
-              (.setCdr node-x (conj (.getCdr node-x) node-y)))
-            (.setInvited node-x true)))
-        (if (not (nil? node-y))
-          (add-new-customer node-y)))))
+  (let [node-x (find-customer x)
+        node-y (if (empty? (has-already-invited y))
+                 (Node. y false nil)
+                 nil)
+        list-y (if (not (nil? node-y))
+                 (list node-y))]
+    (do
+      (if (nil? node-x)
+        (add-new-customer (Node. x true list-y))
+        (do
+          (if (not (nil? node-y))
+            (.setCdr node-x (conj (.getCdr node-x) node-y)))
+          (.setInvited node-x true)))
+      (if (not (nil? node-y))
+        (add-new-customer node-y)))))
 
 (defn read-invite-file 
   "Read invites by customers from text file."
   [path]
   (with-open [rdr (io/reader path)]
     (doseq [line (line-seq rdr)]
-      (let [list (str/split line #"\s")] 
-        (do
-          (let [x (nth list 0) y (nth list 1)]
-            (add-new-invite x y)))))))
+      (let [list (str/split line #"\s") 
+            x (nth list 0) 
+            y (nth list 1)]
+            (add-new-invite x y)))))
 
 (defn count-points-ranking 
   "map customer and reward value."
-  {}
   [customer level]
   (let [invites (.getCdr customer)]
     (loop [sum 0 i 0]
       (if (= i (count invites))
         sum
-        (recur
+        (recur 
           (+ sum 
              (let [invite (nth invites i)
                    chidren-invite (.getCdr invite)]
                   (if (.getInvited invite)
                     (+ (math/expt 0.5 level)
                        (count-points-ranking invite (inc level)))
-                    0))) (inc i))))))
+                    0))) 
+          (inc i))))))
 
 (defn list-ranking 
   "List Ranking values from customer map."
